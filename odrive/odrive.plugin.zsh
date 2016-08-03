fa32416abce0f37bd8b17f4180e76852ee550c34
Chino T <efraazul@gmail.com>
2016-08-03 02:53:09 -0500

    Simple changes

--- odrive/odrive.plugin.zsh
+++ odrive/odrive.plugin.zsh
@@ -1,5 +1,2 @@
-
 alias odriveagent='nohup odriveagent > /dev/null &'
 alias odrivesyncall='for i in *.cloud; do odrive sync $i; done'
-#alias odriveunsyncall='for i in find . -maxdepth 1 -type f; echo "HELLO $i"; done'
-#alias odriveunsyncall='find . -maxdepth 1 -type f | xargs echo "HELLO"'
