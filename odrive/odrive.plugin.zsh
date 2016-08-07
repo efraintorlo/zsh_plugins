5390dc92762751c19d062bb6025a3d3e90aba40a
Chino T <efraazul@gmail.com>
2016-08-07 10:01:11 -0500

    Aliases changed into functions for sake of readability

--- odrive/odrive.plugin.zsh
+++ odrive/odrive.plugin.zsh
@@ -4,8 +4,28 @@ export ODRIVE_ROOT=$HOME/.odrive-agent
 export ODRIVE=$ODRIVE_ROOT/bin/odrive
 export ODRIVEAGENT=$ODRIVE_ROOT/bin/odriveagent
 
-alias odriveagent='nohup $ODRIVEAGENT > /dev/null &; echo $! >> $HOME/.odrive-agent/log/pid.log'
-alias odrivesyncall='for i in *.cloud; do $ODRIVE sync $i; done'
+#alias odriveagent='nohup $ODRIVEAGENT > /dev/null &; echo $! > $HOME/.odrive-agent/log/pid.log'
+#alias odrivesyncall='for i in *.cloud; do $ODRIVE sync $i; done'
+
+function odriveagent() {
+    nohup $ODRIVEAGENT > /dev/null &
+    echo $! > $HOME/.odrive-agent/log/pid.log
+}
+
+function  odrivesyncall() {
+    for i in *.cloud*; do
+        $ODRIVE sync $i;
+    done
+}
+
+function odriveunsyncall() {
+    for filename in ./*; do
+        if [[ -f $filename ]] && [[ $filename != *.cloud* ]]; then 
+            $ODRIVE unsync "$filename"
+        fi
+    done
+}
+
 
 # Check if odriveagent is running
 if [[ -f $ODRIVE_ROOT/log/pid.log ]]
@@ -19,10 +39,10 @@ fi
 function odrive() {
     if [[ $@ == "shutdown" ]]; then
         $ODRIVE shutdown
-        rm $HOME/.odrive-agent/log/pid.log
+        rm -f $HOME/.odrive-agent/log/pid.log
     elif [[ $@ == "reset" ]]; then
         $ODRIVE shutdown
-        rm $HOME/.odrive-agent/log/pid.log
+        rm -f $HOME/.odrive-agent/log/pid.log
         odriveagent
     elif [[ $@ == "pid" ]]; then
         echo "\nOdrive runs with PID: $(cat $ODRIVE_ROOT/log/pid.log)"
